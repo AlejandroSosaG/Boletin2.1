0cb29ab01e11a951ee825a75c49ad2895f8ba89b
asosa <asosa@106-30.info2>
2022-10-10 09:33:00 +0200

    Cambios

--- Tema2_Boletin1/src/tema2_boletin1/Ejercicio9.java
+++ Tema2_Boletin1/src/tema2_boletin1/Ejercicio9.java
@@ -2,73 +2,35 @@ package tema2_boletin1;
 
 import java.util.Scanner;
 
-public class Ejercicio9 {
+public class Ejercicio9 { 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Creamos las dos variable que pediremos a los usuarios.
-		String jugador1;
-		String jugador2;
+		final int PIEDRA = 1;
+		final int PAPEL = 2;
+		final int TIJERAS = 3;
+		int jugador1 = 0;
+		int jugador2 = 0;
+		
 		// Generación de escaner.
 		Scanner sc = new Scanner(System.in);
 		// Pedimos al primer usuario que escriba piedra papel o tijera.
-		System.out.println("Escriba piedra, papel o tijera");
+		System.out.println("Escriba piedra (1), papel (2) o tijeras (3)");
 		// Asignamos a la variable jugador1 el valor elegido por el usuario.
-		jugador1=sc.next();
+		jugador1=sc.nextInt();
+		
 		// Pedimos al segundo usuario que escriba piedra, papel o tijera.
-		System.out.println("Escriba piedra, papel o tijera");
+		System.out.println("Escriba piedra (1), papel (2) o tijeras (3)");
 		// Asignamos a la variable jugador2 el valor elegido por el usuario.
-		jugador2= sc.next();
-		// Si el valor de jugador1 es piedra.
-		if(jugador1=="piedra") {
-			// Si el valor de jugador2 es tijera mostramos mensaje diciendo que gana el primer usuario.
-			if(jugador2=="tijera") {
-				System.out.println("Gana el jugador número 1");
-			// Sino seguimos con el programa.
-			}else {
-				// Si el valor de jugador2 es papel mostramos mensaje diciendo que gana el segundo usuario.
-				if(jugador2=="papel") {
-					System.out.println("Gana el jugador número 2");
-				// Sino mostramos mensaje diciendo que es empate.
-				}else {
-					System.out.println("Empate");
-				
-				}
-			}
-		// Sino seguimos el programa.
-		}else {
-			// Si el valor de jugador1 es tijera.
-			if(jugador1=="tijera") {
-				// Si el valor de jugador2 es tijera mostramos mensaje diciendo que es empate.
-				if(jugador2=="tijera") {
-					System.out.println("Empate");
-				// Sino seguimos el programa.
-				}else {
-					// Si el valor de jugador2 es piedra mostramos mensaje diciendo que gana el segundo usuario.
-					if(jugador2=="piedra") {
-						System.out.println("Gana el jugador número 2");
-					// Sino mostramos mensaje diciendo que gana el primer usuario.
-					}else {
-						System.out.println("Gana el jugador número 1");
-					}
-				}
-			// Sino seguimos el programa.
-			}else {
-				// Si el valor de jugador2 es piedra mostramos mensaje diciendo que gana el primer usuario.
-				if(jugador2=="piedra") {
-					System.out.println("Gana el jugador número 1");
-				// Sino seguimos el programa.
-				}else {
-					// Si el valor de jugador2 es tijera mostramos mensaje diciendo que gana el segundo usuario.
-					if(jugador2=="tijera") {
-						System.out.println("Gana el jugador número 2");
-					// Sino mostramos mensaje diciendo que gana el segundo usuario.
-					}else {
-						System.out.println("Empate");
-					}
-				}
-			}
-		}
+		jugador2= sc.nextInt();
+		
+		System.out.println(jugador1 + " " + jugador2);
+		if(jugador1==jugador2) {
+			System.out.println("Empate");
+		}else if(jugador1==PIEDRA && jugador2==TIJERAS || jugador1==PAPEL && jugador2==PIEDRA || jugador1==TIJERAS && jugador2==PAPEL) {
+			System.out.println("Jugador 1 gana");
+		}else System.out.println("Jugador 2 gana");
 		// Cerramos escaner.
 		sc.close();
 	}
